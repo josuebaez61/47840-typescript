69fdeaa82c617c057f7a239868a27a1738b94e3b
Josue Baez <josue.baez@coderhouse.com>
2023-06-22 21:10:38 -0300

    ADD, typescript explicacion

--- index.ts
+++ index.ts
@@ -1,28 +1,142 @@
 let cantidadAlumnos: number = 0;
-let frutas: (string | number)[] = ['Manzana', 'Naranja', 2];
-let productos: Array<string> = ['Zapato', 'PS5'];
+let frutas: (string | number)[] = ["Manzana", "Naranja", 2];
+let productos: Array<string> = ["Zapato", "PS5"];
 
 let usuario: Humano = {
-    nombre: 'Josue',
-    apellido: 'Baez',
-    edad: 28,
-}
+  nombre: "Josue",
+  apellido: "Baez",
+  edad: 28,
+};
 
 interface Humano {
-    nombre: string;
-    apellido: string;
-    edad: number;
+  nombre: string;
+  apellido: string;
+  edad: number;
 }
 
 interface SuperHeroe extends Humano {
-    poderes: string[];
+  poderes: string[];
 }
 
 function saludarHumano(usuario: Humano): void {
-    console.log(`Hola ${usuario.nombre}`);
+  console.log(`Hola ${usuario.nombre}`);
 }
 function listarSuperpoderes(heroe: SuperHeroe): void {
-    console.log(heroe.poderes.join(', '));
+  console.log(heroe.poderes.join(", "));
 }
 
 saludarHumano(usuario);
+
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+// Tipos genericos
+
+interface ApiUser {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
+
+interface Producto {
+  nombre: string;
+  precio: number;
+}
+
+interface RespuestaListado<Argumento> {
+  data: Argumento[];
+}
+
+const respuestaProductos: RespuestaListado<string> = {
+  data: ["Hola", "Mundo"],
+};
+
+const respuestaApiUsers: RespuestaListado<ApiUser> = {
+  data: [
+    {
+      id: 1,
+      avatar: "",
+      email: "",
+      first_name: "",
+      last_name: "",
+    },
+  ],
+};
+
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+// Clases
+
+interface IVehiculo {
+  marca: string;
+  cantidadRuedas: number;
+  color: string;
+  esMotorizado: boolean;
+  detenerse: () => void;
+}
+
+class Vehiculo implements IVehiculo {
+  marca: string;
+  cantidadRuedas: number;
+  color: string;
+  esMotorizado: boolean;
+  constructor(
+    marca: string,
+    cantidadRuedas: number,
+    color: string,
+    esMotorizado?: boolean
+  ) {
+    this.marca = marca;
+    this.cantidadRuedas = cantidadRuedas;
+    this.color = color;
+    this.esMotorizado = esMotorizado || true;
+  }
+
+  acelerar(): void {}
+
+  detenerse() {
+    
+  };
+}
+
+class VehiculoFormaCorta {
+  constructor(
+    public marca: string,
+    public cantidadRuedas: number,
+    private color: string,
+    public esMotorizado?: boolean
+  ) {}
+
+  establecerColor(color: string): void {
+    this.color = color;
+  }
+}
+
+const limusina = new Vehiculo("Mercedes Benz", 6, "Negro");
+const tractor = new VehiculoFormaCorta("Mercedes Benz", 4, "Verde", true);
+
+console.log(limusina);
+
+tractor.establecerColor("Celeste");
+console.log(tractor);
+
+class Taza {
+  constructor(color: string, materialContruccion = "vidrio") {}
+}
+
+class Motocicleta extends Vehiculo {
+  constructor(
+    marca: string,
+    color: string,
+  ) {
+    super(marca, 2, color, true);
+  }
+}
+
+const motocicletaYamaha = new Motocicleta('Yamaha', 'roja');
+
+motocicletaYamaha.acelerar()
+console.log('motocicletaYamaha', motocicletaYamaha)
--- public/dist/index.js
+++ public/dist/index.js
@@ -1,16 +1,91 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var cantidadAlumnos = 0;
-var frutas = ['Manzana', 'Naranja', 2];
-var productos = ['Zapato', 'PS5'];
+var frutas = ["Manzana", "Naranja", 2];
+var productos = ["Zapato", "PS5"];
 var usuario = {
-    nombre: 'Josue',
-    apellido: 'Baez',
+    nombre: "Josue",
+    apellido: "Baez",
     edad: 28,
 };
 function saludarHumano(usuario) {
     console.log("Hola " + usuario.nombre);
 }
 function listarSuperpoderes(heroe) {
-    console.log(heroe.poderes.join(', '));
+    console.log(heroe.poderes.join(", "));
 }
 saludarHumano(usuario);
+var respuestaProductos = {
+    data: ["Hola", "Mundo"],
+};
+var respuestaApiUsers = {
+    data: [
+        {
+            id: 1,
+            avatar: "",
+            email: "",
+            first_name: "",
+            last_name: "",
+        },
+    ],
+};
+var Vehiculo = /** @class */ (function () {
+    function Vehiculo(marca, cantidadRuedas, color, esMotorizado) {
+        this.marca = marca;
+        this.cantidadRuedas = cantidadRuedas;
+        this.color = color;
+        this.esMotorizado = esMotorizado || true;
+    }
+    Vehiculo.prototype.acelerar = function () { };
+    Vehiculo.prototype.detenerse = function () {
+    };
+    ;
+    return Vehiculo;
+}());
+var VehiculoFormaCorta = /** @class */ (function () {
+    function VehiculoFormaCorta(marca, cantidadRuedas, color, esMotorizado) {
+        this.marca = marca;
+        this.cantidadRuedas = cantidadRuedas;
+        this.color = color;
+        this.esMotorizado = esMotorizado;
+    }
+    VehiculoFormaCorta.prototype.establecerColor = function (color) {
+        this.color = color;
+    };
+    return VehiculoFormaCorta;
+}());
+var limusina = new Vehiculo("Mercedes Benz", 6, "Negro");
+var tractor = new VehiculoFormaCorta("Mercedes Benz", 4, "Verde", true);
+console.log(limusina);
+tractor.establecerColor("Celeste");
+console.log(tractor);
+var Taza = /** @class */ (function () {
+    function Taza(color, materialContruccion) {
+        if (materialContruccion === void 0) { materialContruccion = "vidrio"; }
+    }
+    return Taza;
+}());
+var Motocicleta = /** @class */ (function (_super) {
+    __extends(Motocicleta, _super);
+    function Motocicleta(marca, color) {
+        return _super.call(this, marca, 2, color, true) || this;
+    }
+    return Motocicleta;
+}(Vehiculo));
+var motocicletaYamaha = new Motocicleta('Yamaha', 'roja');
+motocicletaYamaha.acelerar();
+console.log('motocicletaYamaha', motocicletaYamaha);
